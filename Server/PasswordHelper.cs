63db938c5a0bdd3773cbe133d16349a55e50c3ee
Morozov Stanislav <libertadintegra@gmail.com>
2025-11-11 19:43:51 +0200

    Add PasswordHelper class

---
+++ Server/PasswordHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class PasswordHelper
+    {
+        public static (string hash, string salt) HashPassword(string password)
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            var salt = Convert.ToBase64String(hmac.Key);
+            var hash = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password, 0, password.Length)));
+            return (hash, salt);
+        }
+        public static bool VerifyPassword(string password, string hash, string salt)
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            var computed = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            return computed == hash;
+        }
+    }
+}
