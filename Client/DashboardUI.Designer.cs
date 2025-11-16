72278f455e0b458a1c3b69446ef1ac62050bb38b
Mykola Lavrynovych <mykola0073008@gmail.com>
2025-11-16 03:04:39 +0200

    sync

--- Client/DashboardUI.Designer.cs
+++ Client/DashboardUI.Designer.cs
@@ -56,6 +56,26 @@
             pictureBox8 = new PictureBox();
             pictureBox9 = new PictureBox();
             pictureBox10 = new PictureBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            label20 = new Label();
+            label21 = new Label();
+            label22 = new Label();
+            label23 = new Label();
+            label24 = new Label();
             panel1.SuspendLayout();
             panel5.SuspendLayout();
             panel9.SuspendLayout();
@@ -164,6 +184,8 @@
             // panel13
             // 
             panel13.BackColor = Color.FromArgb(0, 25, 50);
+            panel13.Controls.Add(label23);
+            panel13.Controls.Add(label13);
             panel13.Controls.Add(pictureBox9);
             panel13.Location = new Point(28, 72);
             panel13.Name = "panel13";
@@ -185,6 +207,8 @@
             // panel11
             // 
             panel11.BackColor = Color.FromArgb(0, 25, 50);
+            panel11.Controls.Add(label24);
+            panel11.Controls.Add(label14);
             panel11.Controls.Add(pictureBox10);
             panel11.Location = new Point(28, 72);
             panel11.Name = "panel11";
@@ -206,6 +230,8 @@
             // panel12
             // 
             panel12.BackColor = Color.FromArgb(0, 25, 50);
+            panel12.Controls.Add(label15);
+            panel12.Controls.Add(label5);
             panel12.Controls.Add(pictureBox1);
             panel12.Location = new Point(28, 72);
             panel12.Name = "panel12";
@@ -215,6 +241,8 @@
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(0, 25, 50);
+            panel2.Controls.Add(label16);
+            panel2.Controls.Add(label6);
             panel2.Controls.Add(pictureBox2);
             panel2.Location = new Point(381, 72);
             panel2.Name = "panel2";
@@ -224,6 +252,8 @@
             // panel3
             // 
             panel3.BackColor = Color.FromArgb(0, 25, 50);
+            panel3.Controls.Add(label18);
+            panel3.Controls.Add(label8);
             panel3.Controls.Add(pictureBox4);
             panel3.Location = new Point(1086, 72);
             panel3.Name = "panel3";
@@ -233,6 +263,8 @@
             // panel4
             // 
             panel4.BackColor = Color.FromArgb(0, 25, 50);
+            panel4.Controls.Add(label17);
+            panel4.Controls.Add(label7);
             panel4.Controls.Add(pictureBox3);
             panel4.Location = new Point(733, 72);
             panel4.Name = "panel4";
@@ -242,6 +274,8 @@
             // panel6
             // 
             panel6.BackColor = Color.FromArgb(0, 25, 50);
+            panel6.Controls.Add(label21);
+            panel6.Controls.Add(label11);
             panel6.Controls.Add(pictureBox7);
             panel6.Location = new Point(733, 72);
             panel6.Name = "panel6";
@@ -251,6 +285,8 @@
             // panel7
             // 
             panel7.BackColor = Color.FromArgb(0, 25, 50);
+            panel7.Controls.Add(label22);
+            panel7.Controls.Add(label12);
             panel7.Controls.Add(pictureBox8);
             panel7.Location = new Point(1086, 72);
             panel7.Name = "panel7";
@@ -260,6 +296,8 @@
             // panel8
             // 
             panel8.BackColor = Color.FromArgb(0, 25, 50);
+            panel8.Controls.Add(label20);
+            panel8.Controls.Add(label10);
             panel8.Controls.Add(pictureBox6);
             panel8.Location = new Point(381, 72);
             panel8.Name = "panel8";
@@ -269,6 +307,8 @@
             // panel14
             // 
             panel14.BackColor = Color.FromArgb(0, 25, 50);
+            panel14.Controls.Add(label19);
+            panel14.Controls.Add(label9);
             panel14.Controls.Add(pictureBox5);
             panel14.Location = new Point(28, 72);
             panel14.Name = "panel14";
@@ -355,6 +395,266 @@
             pictureBox10.TabIndex = 1;
             pictureBox10.TabStop = false;
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(130, 40);
+            label5.Margin = new Padding(0);
+            label5.Name = "label5";
+            label5.Size = new Size(197, 45);
+            label5.TabIndex = 3;
+            label5.Text = "1000000.00";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(130, 40);
+            label6.Margin = new Padding(0);
+            label6.Name = "label6";
+            label6.Size = new Size(197, 45);
+            label6.TabIndex = 4;
+            label6.Text = "1000000.00";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(130, 40);
+            label7.Margin = new Padding(0);
+            label7.Name = "label7";
+            label7.Size = new Size(197, 45);
+            label7.TabIndex = 4;
+            label7.Text = "1000000.00";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(130, 40);
+            label8.Margin = new Padding(0);
+            label8.Name = "label8";
+            label8.Size = new Size(197, 45);
+            label8.TabIndex = 4;
+            label8.Text = "1000000.00";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(130, 40);
+            label9.Margin = new Padding(0);
+            label9.Name = "label9";
+            label9.Size = new Size(197, 45);
+            label9.TabIndex = 4;
+            label9.Text = "1000000.00";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(130, 40);
+            label10.Margin = new Padding(0);
+            label10.Name = "label10";
+            label10.Size = new Size(197, 45);
+            label10.TabIndex = 4;
+            label10.Text = "1000000.00";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label11.ForeColor = Color.White;
+            label11.Location = new Point(130, 40);
+            label11.Margin = new Padding(0);
+            label11.Name = "label11";
+            label11.Size = new Size(197, 45);
+            label11.TabIndex = 4;
+            label11.Text = "1000000.00";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label12.ForeColor = Color.White;
+            label12.Location = new Point(130, 40);
+            label12.Margin = new Padding(0);
+            label12.Name = "label12";
+            label12.Size = new Size(197, 45);
+            label12.TabIndex = 4;
+            label12.Text = "1000000.00";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.Transparent;
+            label13.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label13.ForeColor = Color.White;
+            label13.Location = new Point(130, 40);
+            label13.Margin = new Padding(0);
+            label13.Name = "label13";
+            label13.Size = new Size(197, 45);
+            label13.TabIndex = 4;
+            label13.Text = "1000000.00";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.Transparent;
+            label14.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label14.ForeColor = Color.White;
+            label14.Location = new Point(130, 40);
+            label14.Margin = new Padding(0);
+            label14.Name = "label14";
+            label14.Size = new Size(197, 45);
+            label14.TabIndex = 4;
+            label14.Text = "1000000.00";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.BackColor = Color.Transparent;
+            label15.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label15.ForeColor = Color.White;
+            label15.Location = new Point(130, 120);
+            label15.Margin = new Padding(0);
+            label15.Name = "label15";
+            label15.Size = new Size(201, 30);
+            label15.TabIndex = 4;
+            label15.Text = "This Year's Expences";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.BackColor = Color.Transparent;
+            label16.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label16.ForeColor = Color.White;
+            label16.Location = new Point(130, 120);
+            label16.Margin = new Padding(0);
+            label16.Name = "label16";
+            label16.Size = new Size(201, 30);
+            label16.TabIndex = 5;
+            label16.Text = "This Year's Expences";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.BackColor = Color.Transparent;
+            label17.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label17.ForeColor = Color.White;
+            label17.Location = new Point(130, 120);
+            label17.Margin = new Padding(0);
+            label17.Name = "label17";
+            label17.Size = new Size(201, 30);
+            label17.TabIndex = 5;
+            label17.Text = "This Year's Expences";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.BackColor = Color.Transparent;
+            label18.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label18.ForeColor = Color.White;
+            label18.Location = new Point(130, 120);
+            label18.Margin = new Padding(0);
+            label18.Name = "label18";
+            label18.Size = new Size(201, 30);
+            label18.TabIndex = 5;
+            label18.Text = "This Year's Expences";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.BackColor = Color.Transparent;
+            label19.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label19.ForeColor = Color.White;
+            label19.Location = new Point(130, 120);
+            label19.Margin = new Padding(0);
+            label19.Name = "label19";
+            label19.Size = new Size(201, 30);
+            label19.TabIndex = 5;
+            label19.Text = "This Year's Expences";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.BackColor = Color.Transparent;
+            label20.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label20.ForeColor = Color.White;
+            label20.Location = new Point(130, 120);
+            label20.Margin = new Padding(0);
+            label20.Name = "label20";
+            label20.Size = new Size(201, 30);
+            label20.TabIndex = 5;
+            label20.Text = "This Year's Expences";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.BackColor = Color.Transparent;
+            label21.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label21.ForeColor = Color.White;
+            label21.Location = new Point(130, 120);
+            label21.Margin = new Padding(0);
+            label21.Name = "label21";
+            label21.Size = new Size(201, 30);
+            label21.TabIndex = 5;
+            label21.Text = "This Year's Expences";
+            // 
+            // label22
+            // 
+            label22.AutoSize = true;
+            label22.BackColor = Color.Transparent;
+            label22.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label22.ForeColor = Color.White;
+            label22.Location = new Point(130, 120);
+            label22.Margin = new Padding(0);
+            label22.Name = "label22";
+            label22.Size = new Size(201, 30);
+            label22.TabIndex = 5;
+            label22.Text = "This Year's Expences";
+            // 
+            // label23
+            // 
+            label23.AutoSize = true;
+            label23.BackColor = Color.Transparent;
+            label23.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label23.ForeColor = Color.White;
+            label23.Location = new Point(130, 120);
+            label23.Margin = new Padding(0);
+            label23.Name = "label23";
+            label23.Size = new Size(201, 30);
+            label23.TabIndex = 5;
+            label23.Text = "This Year's Expences";
+            // 
+            // label24
+            // 
+            label24.AutoSize = true;
+            label24.BackColor = Color.Transparent;
+            label24.Font = new Font("Segoe UI", 7.875F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label24.ForeColor = Color.White;
+            label24.Location = new Point(130, 120);
+            label24.Margin = new Padding(0);
+            label24.Name = "label24";
+            label24.Size = new Size(201, 30);
+            label24.TabIndex = 5;
+            label24.Text = "This Year's Expences";
+            // 
             // DashboardUI
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
@@ -373,17 +673,27 @@
             panel9.ResumeLayout(false);
             panel9.PerformLayout();
             panel13.ResumeLayout(false);
+            panel13.PerformLayout();
             panel10.ResumeLayout(false);
             panel10.PerformLayout();
             panel11.ResumeLayout(false);
+            panel11.PerformLayout();
             panel12.ResumeLayout(false);
+            panel12.PerformLayout();
             panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             panel6.ResumeLayout(false);
+            panel6.PerformLayout();
             panel7.ResumeLayout(false);
+            panel7.PerformLayout();
             panel8.ResumeLayout(false);
+            panel8.PerformLayout();
             panel14.ResumeLayout(false);
+            panel14.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
@@ -427,5 +737,25 @@
         private PictureBox pictureBox5;
         private PictureBox pictureBox9;
         private PictureBox pictureBox10;
+        private Label label5;
+        private Label label7;
+        private Label label8;
+        private Label label6;
+        private Label label11;
+        private Label label12;
+        private Label label10;
+        private Label label9;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label17;
+        private Label label18;
+        private Label label16;
+        private Label label21;
+        private Label label22;
+        private Label label20;
+        private Label label19;
+        private Label label23;
+        private Label label24;
     }
 }
