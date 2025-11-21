61a5cc89404bc38e1e1551e9f8ff323642ad1e22
Misha Kovalov <kovamu0b@gmail.com>
2025-11-21 23:55:01 +0200

    Create LogEntry.cs

---
+++ Domain/LogEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class LogEntry
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Event { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
+
